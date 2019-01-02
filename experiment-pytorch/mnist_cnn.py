d010e509f172e55dfb52266fa433bdb3b1fc2c8a
Yuval Greenfield <ubershmekel@gmail.com>
2019-01-02 13:46:31 -0800

    Fix deprecations and pytorch 1.0 changes

--- experiment-pytorch/mnist_cnn.py
+++ experiment-pytorch/mnist_cnn.py
@@ -24,7 +24,7 @@ parser.add_argument(
 parser.add_argument(
     '--epochs',
     type=int,
-    default=3,
+    default=2,
     metavar='N',
     help='number of epochs to train (default: 10)')
 parser.add_argument(
@@ -105,7 +105,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
 
 
 model = Net()
@@ -129,7 +129,7 @@ def train(epoch):
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+                100. * batch_idx / len(train_loader), loss.item()))
 
 
 def test():
@@ -139,13 +139,14 @@ def test():
     for data, target in test_loader:
         if args.cuda:
             data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += F.nll_loss(
-            output, target, size_average=False).data[0]  # sum up batch loss
-        pred = output.data.max(
-            1, keepdim=True)[1]  # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        data, target = Variable(data), Variable(target)
+        with torch.no_grad():
+            output = model(data)
+            test_loss += F.nll_loss(
+                output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.data.max(
+                1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(test_loader.dataset)
     print(
